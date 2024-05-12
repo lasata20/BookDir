796acdd7014e6dd1b66dae24f3941f881c7f708e
Lasata Shrestha <lasatastha20@gmail.com>
2024-05-12 16:51:52 +0545

    add error messages

---
+++ controllers/errorController.js
@@ -0,0 +1,32 @@
+const AppError = require('../utils/appError');
+
+const handleCastErrorDB = err => {
+    const message = `Invalid ${err.path}: ${err.value}.`;
+    return new AppError(message, 400);
+};
+
+const handleDuplicateFieldDB = err => {
+    const value = err.errmsg.match(/([" '])(\\?.)*?\1/)[0];
+
+    const message = `Duplicate field value: ${value}. Please use another value.`;
+    return new AppError(message, 400);
+};
+
+const handleValidationErrorDB = err => {
+    const errors = Object.values(err.errors).map(el => el.message);
+
+    const message = `Invalid input data. ${errors.join('. ')}`;
+    return new AppError(message, 400);
+};
+
+const handleJWTError = err => new AppError('Invalid Token. Please login again!', 401);
+
+const sendErrorDev = (err, res) => {
+    res.status(err.statusCode).json({
+        status: err.status,
+        error: err,
+        message: err.message,
+        stack: err.stack
+    });
+};
+
