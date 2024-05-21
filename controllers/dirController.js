20b77f05233920007035f841b1a68297a3e1cd59
Lasata Shrestha <lasatastha20@gmail.com>
2024-05-21 13:06:32 +0545

    add multer to upload files

--- controllers/dirController.js
+++ controllers/dirController.js
@@ -1,63 +1,115 @@
 // const fs = require('fs');
 const mongoose = require('mongoose');
+const multer =require('multer');
 const Book = require('../models/dirModel');
 const catchAsync = require('../utils/catchAsync');
+const AppError = require('../utils/appError');
 
 
 // const books = JSON.parse(
 //     fs.readFileSync(`./dev-data/data/books-simple.json`)
 // );
 
-exports.getAllBooks = catchAsync(async (req, res) => {
-        const books = await Book.find();
 
-        res.status(200).json({
-            status: "success",
-            results: books.length,
-            data:{
-                books
-            }
-        });
+const multerStorage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'public/img/books');
+    },
+    filename: (req, file, cb) => {
+        cb(null, `book-${Date.now()}.jpg`);
+    }
 });
 
-exports.getBookById = catchAsync(async (req, res) => {
-        const book = await Book.findById(req.params.id);
+const multerFilter = (req, files, cb) => {
+//     if (file.mimetype.startsWith('image')) {
+//         cb(null, true)
+//     } else {
+//         cb(new AppError('Not an image! Please upload image', 400), false);
+//     }
+    const ext = path.extname(files.originalname);
+    if (ext !== '.png' && ext !== '.jpg' && ext !== '.jpeg' && ext !== '.pdf') {
+        return cb(new AppError('Only images and pdf files allowed'));
+    }
+        cb(null, true);
 
-        res.status(200).json({
-            status: "success",
-            data:{
-                book
-            }
-        });
+
+}
+
+const upload = multer({ 
+    storage: multerStorage,
+    fileFilter: multerFilter
+});
+
+exports.uploadPhoto =upload.single('image');
+
+exports.getAllBooks = catchAsync(async (req, res) => {
+    const books = await Book.find();
+
+    res.status(200).json({
+        status: "success",
+         results: books.length,
+        data:{
+                books
+        }
+    });
+});
+
+exports.getBookById = catchAsync(async (req, res, next) => {
+    const book = await Book.findById(req.params.id);
+
+        // if(!book) {
+        //     return next(new AppError('No Book found', 404))
+        // }
+
+    res.status(200).json({
+        status: "success",
+        data:{
+            book
+        }
+    });
 });
 
 exports.createBook =  catchAsync(async (req, res) => {
-        const newBook = await Book.create(req.body);
+    const image = req.file.filename;
+    const newBookData = {
+        ...req.body,
+        image
+    };
 
-        res.status(201).json({
-            status: 'success',
-            data: {
-                book: newBook
-            }
-        });
+    const newBook = await Book.create(newBookData);
+
+    res.status(201).json({
+        status: 'success',
+        data: {
+            book: newBook
+        }
+    });
 });
 
-exports.updateBook = catchAsync(async (req, res) => {
-        const book = await Book.findByIdAndUpdate(req.params.id, req.body, {
-            new: true,
-            runValidators: true
-        })
+exports.updateBook = catchAsync(async (req, res, next) => {
+    const book = await Book.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+        runValidators: true
+    })
 
-        res.status(200).json({
-            status: 'success',
-            data: {
-                book
-            }
-        })
+    if(!book) {
+        return next(new AppError('No Book found', 404))
+    }
+
+    res.status(200).json({
+        status: 'success',
+        data: {
+            book
+        }
+    })
 });
 
-exports.deleteBook =  catchAsync(async (req, res) => {
-    await Book.findByIdAndDelete(req.params.id, req.body);
+exports.deleteBook =  catchAsync(async (req, res, next) => {
+    const book = await Book.findByIdAndDelete(req.params.id, req.body);
+
+    if(!book) {
+        return next(new AppError('No Book found', 404))
+    }
 
     res.status(200).json({
         status: 'success',
